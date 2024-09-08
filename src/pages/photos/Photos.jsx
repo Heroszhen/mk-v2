206eb9772a69cbbffdb003bcf406f42292f3cdf4
zhen <zhen.yang@hotmail.fr>
2024-09-08 16:03:14 +0200

    maj

--- src/pages/photos/Photos.jsx
+++ src/pages/photos/Photos.jsx
@@ -1,9 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import usePhotosStore from '../../store/photosStore';
+import Search from '../../components/search/Search';
 
 const Photos = (props) => {
+    const {photos, itemsByPage, total, page, searchPhotos, keywordsInStore} = usePhotosStore();
+    const [keywords, setKeywords] = useState(keywordsInStore);
+    const [currentPage, setCurrentPage] = useState(page);
+    const [enter, setEnter] = useState(true);
+
+    useEffect(() => {
+        if (enter === true) {
+            setEnter(false);
+            if (photos.length === 0)searchPhotos(keywords, currentPage);
+        } else {
+            searchPhotos(keywords, currentPage);
+        }
+    }, [currentPage]);
+
+    const searchDatas = (key) => {
+        // searchVideos(key, 1);
+    }
+
     return (
-        <section id="photos">
-            photos
+        <section id="photos" data-page="main">
+            <Search keywords={keywords} setKeywords={setKeywords} setCurrentPage={setCurrentPage} searchDatas={searchDatas} />
+            <div className='container'>
+                
+            </div>
         </section>
     )
 }
---
+++ src/store/photosStore.js
@@ -0,0 +1,39 @@
+import {create} from "zustand";
+import { getRequestHeaders, getEnv } from "../services/utils";
+
+const usePhotosStore = create((set, get) => ({
+    photos: [],
+    keywordsInStore: '',
+    page: 1,
+    itemsByPage: 20,
+    total: 0,
+    abortController: null,
+    searchPhotos: async (keywords, page) => {
+        if (get().abortController !== null)get().abortController.abort;
+
+        set((state) => ({
+            canSearch: false, 
+            page: page, 
+            abortController: new AbortController(),
+            keywordsInStore: keywords
+        }));
+
+        const env = await getEnv();
+        fetch(`${env.VITE_API_URL}/photos/search?key=${keywords}&page=${get().page}`, {
+            signal: get().abortController.signal,
+            headers: getRequestHeaders(env)
+        })
+        .then(response => {
+            set((state) => ({canSearch: true}));
+            return response.json();
+        })
+        .then(response => {
+            set((state) => ({
+                photos: response.data,
+                itemsByPage: response.itemsByPage,
+                total: response.total,
+            }));
+        });
+    }
+}));
+export default usePhotosStore;
