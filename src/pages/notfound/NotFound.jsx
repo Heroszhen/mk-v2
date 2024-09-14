c2ce47afec1001bf8dc93ead5d4515817f2d4d9e
zhen <zhen.yang@hotmail.fr>
2024-09-14 16:13:10 +0200

    modify notfound

--- src/pages/notfound/NotFound.jsx
+++ src/pages/notfound/NotFound.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import './notfound.scss';7
+import photo from '../../assets/notfound.gif';
 
 const NotFound = (props) => {
     return (
         <section id="not-found" data-page="main">
-            not found
+            <div className='container'>
+                <div className="row justify-content-center">
+                    <div className="col-12 text-center fs-1 mb-5">
+                        Egaré?
+                    </div>
+                    <div className="col-12 col-md-8 col-lg-4">
+                        <img src={photo} alt='' />
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
